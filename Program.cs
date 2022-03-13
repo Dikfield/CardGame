aa6ee8a93f43468b6e7134e40e1fbaf7cfa117fb
Dikfield <Dinash@outlook.com>
2022-03-13 10:18:57 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Stripe;
+using Stripe.Issuing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using System.Security.AccessControl;
+
+namespace CardGame
+{
+    }
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+}
