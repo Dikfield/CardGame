9de537ec884cafd32aac968d4dafdb3318fe9d0b
Dikfield <Dinash@outlook.com>
2022-03-13 10:20:16 -0300

    Deck Creation

--- Program.cs
+++ Program.cs
@@ -1,15 +1,75 @@
-﻿using Stripe;
-using Stripe.Issuing;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http.Headers;
 using System.Reflection.Metadata.Ecma335;
-using System.Runtime.CompilerServices;
-using System.Security.AccessControl;
+using System.Threading;
 
 namespace CardGame
 {
+    class Card
+    {
+        public int Numbers { get; set; }
+        public string Suits { get; set; }
+        public string Name { get; set; }
+        public Card CardInHand { get; set; }
+
+    }
+    class Deck
+    {
+        public List<Card> DeckOfCards { get; set; }
+        public List<Card> CardInHands { get; set; }
+
+        public Deck()
+        {
+            List<Card> deckOfCards_ = new List<Card>();
+            int[] numbers = new int[13]
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+            string[] suits = new string[4] { "Espada", "Ouro", "Copas", "Paus" };
+            foreach (int number in numbers)
+            {
+                foreach (string suit in suits)
+                {
+                    deckOfCards_.Add(new Card { Numbers = number, Suits = suit, Name = number + " de " + suit });
+                }
+            }
+
+            DeckOfCards = deckOfCards_;
+        }
+
+        public void CardsInHand()
+        {
+            List<Card> cardInHands_ = new List<Card>();
+            int count = 0;
+            foreach (Card card in DeckOfCards)
+            {
+                while (count < 6)
+                {
+                    cardInHands_.Add(new Card {CardInHand = card});
+                    count++;
+                }
+            }
+
+            CardInHands = cardInHands_;
+        }
+        public void ShowDeck()
+        {
+            Console.WriteLine("Deck of cards is: ");
+            foreach (Card card in DeckOfCards)
+            {
+                Console.Write(card.Name + "\n");
+            }
+
+        }
+        public void ShowHand()
+        {
+            Console.WriteLine("Your cards in Hands are: ");
+            Deck cardDeck = new Deck();
+            foreach (Card card in CardInHands)
+            {
+                Console.Write(card.CardInHand + "\n");
+            }
+
+        }
     }
 
     internal class Program
@@ -17,8 +77,10 @@ namespace CardGame
 
         static void Main(string[] args)
         {
-
+            var deck = new Deck();
+            deck.ShowDeck();
+            deck.ShowHand();
         }
     }
-
 }
+
