7920b48fee7b97213eb2bb789bfbf96418e02e69
Dikfield <Dinash@outlook.com>
2022-03-14 13:52:59 -0300

    Last Level commit

---
+++ Hand.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace NewCardGame
+{
+    class Hand
+    {
+        public List<Card> CardsInHand { get;}
+        public List<Card> RestOfDeck { get;}
+        public Hand()
+        {
+            CardsInHand = new List<Card>();
+            RestOfDeck = new List<Card>();
+            var deck = new Deck();
+            for (int i = 0; i < 5; i++)
+            {
+                var topCard = deck.DeckOfCards[0];
+                CardsInHand.Add(topCard);
+                deck.DeckOfCards.RemoveAt(0);
+            }
+
+            RestOfDeck = deck.DeckOfCards;
+
+        }
+    }
+}
